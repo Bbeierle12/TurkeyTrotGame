ab1d56827b637fecf314b9738e861d439b587237
Brandon <Bbeierle21@gmail.com>
2025-11-27 17:37:01 -0800

    Refactor main entry point to use TurkeyTrotDefense component; removed old monolithic component reference.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Refactored component - uses GameEngine for all game logic
 import TurkeyTrotDefense from './TurkeyTrotDefense'
-// Old monolithic component (kept for reference)
-// import TurkeyTrotDefensePhase5 from './TurkeyTrotDefensePhase5'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TurkeyTrotDefense />
